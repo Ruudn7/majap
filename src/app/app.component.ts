a9a3396fe45fd0a6551d0e285f1c1ebf44b38468
Ruudn7 <slawomirwojciga@MacBook-Pro-Sawomir.local>
2024-09-09 17:55:31 +0200

    add simple nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavComponent } from './ui/nav/nav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgbNavModule],
+  imports: [NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
